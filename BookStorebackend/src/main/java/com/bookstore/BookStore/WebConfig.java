0a2d4f46e4a31fa97ff741eee7e4cf2286e87cce
komal Tiwari <tiwarikomal952@gmail.com>
2026-02-21 21:00:47 +0530

    new updation

--- BookStorebackend/src/main/java/com/bookstore/BookStore/WebConfig.java
+++ BookStorebackend/src/main/java/com/bookstore/BookStore/WebConfig.java
@@ -1,6 +1,7 @@
 package com.bookstore.BookStore;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -19,6 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("https://book-net-five.vercel.app") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
